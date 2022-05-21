acc46af692cf72ae3913f973be8b3afc44e3d402
rocioa6 <rocioa6@gmail.com>
2022-05-21 21:43:39 +0200

    Iteration#4

--- es6.js
+++ es6.js
@@ -6,7 +6,7 @@ por consola la suma de los dos parametros.
 1.2 Ejecuta esta función pasando un solo parametro
 1.3 Ejecuta esta función pasando dos parametros */
 let arrowFunction = (a = 10, b = 5) => {
-console.log(a + b);
+  console.log(a + b);
 };
 arrowFunction();
 arrowFunction(3);
@@ -14,12 +14,15 @@ arrowFunction(3, 4);
 
 ///////////////////////
 
-
 /* 2.1 En base al siguiente javascript, crea variables en base a las propiedades 
 del objeto usando object destructuring e imprimelas por consola. Cuidado, 
 no hace falta hacer destructuring del array, solo del objeto. */
 
-const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
+const game = {
+  title: "The last us 2",
+  gender: ["action", "zombie", "survival"],
+  year: 2020,
+};
 let { title, gender, year } = game;
 console.log(title, gender, year);
 
@@ -27,59 +30,108 @@ console.log(title, gender, year);
 llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
 imprimelo por consola. */
 
-const fruits = ['Banana', 'Strawberry', 'Orange'];
-let [ fruit1, fruit2, fruit3 ] = fruits;
+const fruits = ["Banana", "Strawberry", "Orange"];
+let [fruit1, fruit2, fruit3] = fruits;
 console.log(fruit1, fruit2, fruit3);
 
 /* 2.3 En base al siguiente javascript, usa destructuring para crear 2 
 variables igualandolo a la función e imprimiendolo por consola. */
 
 const animalFunction = () => {
-    return {name: 'Bengal Tiger', race: 'Tiger'}
+  return { name: "Bengal Tiger", race: "Tiger" };
 };
 let { name, race } = animalFunction();
-console.log(name, race)
+console.log(name, race);
 
 /* 2.4 En base al siguiente javascript, usa destructuring para crear las 
 variables name y itv con sus respectivos valores. Posteriormente crea 
 3 variables usando igualmente el destructuring para cada uno de los años 
 y comprueba que todo esta bien imprimiendolo. */
 
-const car = {name2: 'Mazda 6', itv: [2015, 2011, 2020] }
+const car = { name2: "Mazda 6", itv: [2015, 2011, 2020] };
 let { name2, itv } = car;
 console.log(`nombre ${name2} itv ${itv}`);
-let [ year1, year2, year3 ] = itv;
+let [year1, year2, year3] = itv;
 console.log(year1, year2, year3);
-
 
 ///////////////////////
 
 /* 3.1 Dado el siguiente array, crea una copia usando spread operators. */
-let pointsList = [32, 54, 21, 64, 75, 43]
+let pointsList = [32, 54, 21, 64, 75, 43];
 let pointListCopy = [...pointsList];
 console.log(pointListCopy);
 
 /* 3.2 Dado el siguiente objeto, crea una copia usando spread operators. */
-let toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
-let toyCopy = {...toy};
+let toy = { name: "Bus laiyiar", date: "20-30-1995", color: "multicolor" };
+let toyCopy = { ...toy };
 console.log(toyCopy);
 
 /* 3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando 
 spread operatos. */
 pointsList = [32, 54, 21, 64, 75, 43];
-let pointsLis2 = [54,87,99,65,32];
+let pointsLis2 = [54, 87, 99, 65, 32];
 let pointListNew = [...pointsList, ...pointsLis2];
 console.log(pointListNew);
 /* 3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos 
 con spread operators. */
-toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
-let toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']};
-let toyNew = {...toy, ...toyUpdate};
+toy = { name: "Bus laiyiar", date: "20-30-1995", color: "multicolor" };
+let toyUpdate = { lights: "rgb", power: ["Volar like a dragon", "MoonWalk"] };
+let toyNew = { ...toy, ...toyUpdate };
 console.log(toyNew);
 
 /* 3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 
 pero sin editar el array inicial. De nuevo, usando spread operatos. */
-let colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+let colors = ["rojo", "azul", "amarillo", "verde", "naranja"];
 let colorsNew = [...colors];
-colorsNew.splice(2,1)
+colorsNew.splice(2, 1);
 console.log(colorsNew);
+
+////////////////////
+
+/* 4.1 Dado el siguiente array, devuelve un array con sus nombres 
+utilizando .map(). */
+let users = [
+  { id: 1, name: "Abel" },
+  { id: 2, name: "Julia" },
+  { id: 3, name: "Pedro" },
+  { id: 4, name: "Amanda" },
+];
+
+let usersName = users.map (user => user.name);
+console.log(usersName);
+
+/* 4.2 Dado el siguiente array, devuelve una lista que contenga los valores 
+de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que 
+empiece por 'A'. */
+users = [
+  { id: 1, name: "Abel" },
+  { id: 2, name: "Julia" },
+  { id: 3, name: "Pedro" },
+  { id: 4, name: "Amanda" },
+];
+let usersName2 = users.map((user) => {
+    let firstLetter = user.name.charAt(0);
+    if (firstLetter === "A") {
+      return "Anacleto";
+    } else return user.name;
+  });
+  console.log(usersName2);
+
+/* 4.3 Dado el siguiente array, devuelve una lista que contenga los valores 
+de la propiedad .name y añade al valor de .name el string ' (Visitado)' 
+cuando el valor de la propiedad isVisited = true. */
+let cities = [
+  { isVisited: true, name: "Tokyo" },
+  { isVisited: false, name: "Madagascar" },
+  { isVisited: true, name: "Amsterdam" },
+  { isVisited: false, name: "Seul" },
+];
+
+let newCities = cities.map( (city) => {
+if (city.isVisited){
+    return city.name += ' (Visitado)';
+}
+else return city.name;
+
+})
+console.log(newCities);
